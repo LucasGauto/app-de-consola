2ede93dd034db0cf9eaef0423c1fea0c6fafb70f
PinoBlue <lucasgauto55555@gmail.com>
2023-05-17 17:26:36 -0300

    Commit

--- usuarios/acciones.py
+++ usuarios/acciones.py
@@ -1,4 +1,7 @@
+import usuarios.usuario as modelo
+
 class Acciones:
+    
     def registro(self):
         print("Ok. Vamos a registrarte en el sistema...")
         
@@ -6,7 +9,14 @@ class Acciones:
         apellidos = input("Cuales son tus apellidos?: ")
         email = input("Ingresa tu email: ")
         password = input("Ingresa una contraseña: ")
-
+        usuario = modelo.Usuario(nombre,apellidos,email,password)
+        registro = usuario.registrar()
+        
+        if registro[0] >= 1:
+            print(f"Perfecto {registro[1].nombre} te has registrado con el email {registro[1].email}")
+        else:
+            print("No te has registrado correctamente.")
+        
     def login(self):
         print("Ok. Identificate en el sistema...")
         email = input("Ingresa tu email: ")
