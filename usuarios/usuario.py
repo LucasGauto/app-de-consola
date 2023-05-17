7fb6fcd4229d7c364003a9a2bf96d8eb143bb1f9
PinoBlue <lucasgauto55555@gmail.com>
2023-05-17 17:25:54 -0300

    Se cambio el execute. Se utilizo un metodo recomendado para prtegerse de SQL injection attacks

--- usuarios/usuario.py
+++ usuarios/usuario.py
@@ -1,3 +1,20 @@
+import pyodbc
+import datetime
+direccion_servidor = 'LAPTOP-TCBDQ8PH\SQLEXPRESS'
+nombre_bd = 'app_consola'
+nombre_usuario ='login1'
+password = '123'
+try:
+    conexion = pyodbc.connect('DRIVER={SQL Server};SERVER=' +
+                            direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
+    print("OK! conexión exitosa")
+except Exception as e:
+    # Atrapar error
+    print("Ocurrió un error al conectar a SQL Server: ", e)
+
+cursor = conexion.cursor()
+#aca irian las consultas
+
 class Usuario:
     def __init__(self, nombre, apellidos, email, password):
         self.nombre = nombre
@@ -5,7 +22,18 @@ class Usuario:
         self.email = email
         self.password = password
 
-    def registar(self):
-        return self.nombre
+    def registrar(self):
+        fecha = datetime.datetime.now()
+        #sql = "INSERT INTO users VALUES(null, %s, %s, %s, %s, %s);"
+        #usuario = (self.nombre, self.apellidos, self.email, self.password, fecha)
+        #cursor.execute(sql % usuario)
+        cursor.execute("""
+            INSERT INTO users
+            VALUES  (?, ?, ?, ?, ?)
+        """, self.nombre, self.apellidos, self.email, self.password, fecha)
+
+        conexion.commit()
+        return [cursor.rowcount, self]
+
     def identificar(self):
         return self.nombre
